3dd3f4792f892e36885ec8662562962beab3c50c
Samuel Roth <sejr@me.com>
2014-07-24 16:38:16 -0400

    Different templates

--- app/views/site/blog/index.blade.php
+++ app/views/site/blog/index.blade.php
@@ -11,17 +11,42 @@
 
 	<div class="col-md-8">
 		@foreach ($posts as $post)
-			<div class="panel panel-default">
-		  	<div class="panel-body">
-			  	{{ $post->image_src }} 
-		    </div>
-		    <div class="panel-footer">
-		    	<h3 class="page-header"><a href="{{{ $post->url() }}}" class="post-title">{{ String::title($post->title) }}</a> <small>Posted {{{ $post->date() }}}</small></h3>
-		      <p>{{ String::tidy(Str::limit($post->content, 200)) }}</p>
-					<hr>
-					<p><a class="btn btn-default btn-lg" href="{{{ $post->url() }}}">Read more <span class="octicon octicon-chevron-right"></span></a></p>
-		    </div>
-		  </div> <!-- .panel panel-default -->
+			@if ($post->image_src != "")
+    		<div class="panel panel-default">
+					<div class="panel-body">
+						<img src="{{ $post->image_src }}">
+					</div>
+					<div class="panel-footer">
+						<h3 class="page-header"><a href="{{{ $post->url() }}}" class="post-title">{{ String::title($post->title) }}</a> <small>Posted {{{ $post->date() }}}</small></h3>
+						<p>{{ String::tidy(Str::limit($post->content, 200)) }}</p>
+						<hr>
+						<p><a class="btn btn-default btn-lg" href="{{{ $post->url() }}}">Read more <span class="octicon octicon-chevron-right"></span></a></p>
+					</div>
+				</div> <!-- .panel panel-default -->
+			@elseif ($post->video_src != "")
+	    	<div class="panel panel-default">
+					<div class="panel-body">
+						<div class="video-container">
+		      		<iframe src="//www.youtube.com/embed/{{ $post->video_src }}" width="560" height="315" frameborder="0" allowfullscreen></iframe>
+		      	</div>
+					</div>
+					<div class="panel-footer">
+						<h3 class="page-header"><a href="{{{ $post->url() }}}" class="post-title">{{ String::title($post->title) }}</a> <small>Posted {{{ $post->date() }}}</small></h3>
+						<p>{{ String::tidy(Str::limit($post->content, 200)) }}</p>
+						<hr>
+						<p><a class="btn btn-default btn-lg" href="{{{ $post->url() }}}">Read more <span class="octicon octicon-chevron-right"></span></a></p>
+					</div>
+				</div> <!-- .panel panel-default -->
+			@else
+	    	<div class="panel panel-default">
+					<div class="panel-body">
+						<h3 class="page-header"><a href="{{{ $post->url() }}}" class="post-title">{{ String::title($post->title) }}</a> <small>Posted {{{ $post->date() }}}</small></h3>
+						<p>{{ String::tidy(Str::limit($post->content, 200)) }}</p>
+						<hr>
+						<p><a class="btn btn-default btn-lg" href="{{{ $post->url() }}}">Read more <span class="octicon octicon-chevron-right"></span></a></p>
+					</div>
+				</div> <!-- .panel panel-default -->
+			@endif
 		@endforeach
 	</div> <!-- .col-md-8 -->
 
