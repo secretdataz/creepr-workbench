5d9927591901381bc9114440c120360fe4f189ae
Samuel Roth <sejr@me.com>
2014-07-24 05:28:06 -0400

    Deployment stuff

--- app/views/site/user/forgot.blade.php
+++ app/views/site/user/forgot.blade.php
@@ -15,7 +15,7 @@
       <div class="panel-body">
         <h3 class="page-header">Forgotten Password</h3>
         <p>We can send you instructions on how to reset your password.</p>
-        
+
         {{ Confide::makeForgotPasswordForm() }}
       </div>
     </div>
