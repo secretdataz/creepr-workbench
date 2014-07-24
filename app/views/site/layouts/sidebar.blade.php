f2db025b8899694f7ac957aecc9b644a432e0bb2
Samuel Roth <sejr@me.com>
2014-07-24 14:30:09 -0400

    Fixed server status

--- app/views/site/layouts/sidebar.blade.php
+++ app/views/site/layouts/sidebar.blade.php
@@ -2,12 +2,14 @@
   <div class="panel-body">
     <div class="page-header"><h4 class="panel-heading">Server Status</h4></div>
       <?php
-        $status = new MinecraftServerStatus();
-        $response = $status->getStatus('us.creepr.co', 25565);
-        if (!$response) {
-          echo '<a class="btn btn-danger btn-lg btn-block" href="#"><span class="octicon octicon-primitive-dot"></span>&nbsp; us.creepr.co</a>';
-        } else {
+        $ip = '184.154.16.178';
+        $port = 25565;
+
+        $status = @fsockopen($ip, $port, $errno, $errstr, 200);
+        if ($status >= 1) {
           echo '<a class="btn btn-success btn-lg btn-block" href="#"><span class="octicon octicon-primitive-dot"></span>&nbsp; us.creepr.co</a>';
+        } else {
+          echo '<a class="btn btn-danger btn-lg btn-block" href="#"><span class="octicon octicon-primitive-dot"></span>&nbsp; us.creepr.co</a>';
         }
       ?>
   </div>
