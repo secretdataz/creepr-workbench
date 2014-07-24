b6f165c4e8fedac6f148e9085b10d82710381996
Samuel Roth <sejr@me.com>
2014-07-24 18:05:16 -0400

    Getting comments to work

--- app/controllers/BlogController.php
+++ app/controllers/BlogController.php
@@ -36,7 +36,7 @@ class BlogController extends BaseController {
 	{
 		// Get all the blog posts
 		$posts = $this->post->orderBy('created_at', 'DESC')->paginate(10);
-    $comments = $post->comments()->orderBy('created_at', 'ASC')->get();
+    $comments = $posts->comments();
 
 
 		// Show the page
