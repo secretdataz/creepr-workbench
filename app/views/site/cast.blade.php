a5d699fd5baa8ff8563995d65e8a3903695b9a22
Samuel Roth <sejr@me.com>
2014-07-25 00:34:28 -0400

    Cast view

--- app/views/site/cast.blade.php
+++ app/views/site/cast.blade.php
@@ -1,6 +1,98 @@
 @extends('site.layouts.default')
-@section('content')
 
-<h1>Cast</h1>
+{{-- Content --}}
+@section('content')
+<div class="row">
+  <div class="col-md-4">
+
+    @include('site.layouts.sidebar')
+
+  </div>
+
+  <div class="col-md-8">
+    <div class="panel panel-default">
+      <div class="panel-body">
+        <div class="page-header"><h3 class="panel-heading">Meet the Cast</h3></div>
+        <h4>Administrators</h4>
+        <div class="row">
+          <div class="col-md-3">
+            <div class="profile">
+              <img class="profile-pic" src="https://minotar.net/helm/sampsonian/100.png">
+              <strong>sampsonian</strong>
+              <p>Sam</p>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="profile">
+              <img class="profile-pic" src="https://minotar.net/helm/Newtzor/100.png">
+              <strong>Newtzor</strong>
+              <p>Brandon</p>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="profile">
+              <img class="profile-pic" src="https://minotar.net/helm/Newby1223/100.png">
+              <strong>Newby1223</strong>
+              <p>Jacob</p>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="profile">
+              <img class="profile-pic" src="https://minotar.net/helm/Nelon_THA_Melon/100.png">
+              <strong>Nelon_THA_Melon</strong>
+              <p>Nelson</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-3">
+            <div class="profile">
+              <img class="profile-pic" src="https://minotar.net/helm/Failmaster9000/100.png">
+              <strong>Failmaster9000</strong>
+              <p>Mike</p>
+            </div>
+          </div>
+        </div>
+
+        <hr>
+
+        <h4>Moderators</h4>
+        <div class="row">
+          <div class="col-md-3">
+            <div class="profile">
+              <img class="profile-pic" src="https://minotar.net/helm/DJ_Flapjack/100.png">
+              <strong>DJ_Flapjack</strong>
+              <p>Daret</p>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="profile">
+              <img class="profile-pic" src="https://minotar.net/helm/Lord_Reekris/100.png">
+              <strong>Lord_Reekris</strong>
+              <p>Aaron</p>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="profile">
+              <img class="profile-pic" src="https://minotar.net/helm/Newby3/100.png">
+              <strong>Newby3</strong>
+              <p>Andy</p>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="profile">
+              <img class="profile-pic" src="https://minotar.net/helm/KylerHolland/100.png">
+              <strong>KylerHolland</strong>
+              <p>Kyler</p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+</div> <!-- .row -->
 
 @stop
